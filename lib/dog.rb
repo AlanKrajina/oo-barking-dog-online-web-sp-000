cf4cf756a469284b1ce77be4198a4ed463f232a6
Alan Krajina <alankrajina88@gmail.com>
2019-05-15 17:39:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    @this_name=dog_name
+  end
+  
+  def name
+    @this_name
+  end
+  
+  def bark=(bark)
+    @this_bark=dog_name
+  end
+  
+  def bark
+    @this_name
+  end 
+  
+end
